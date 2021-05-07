d03f4b3a1291844f7262c219e5d4cf0b27f77d8f
S. Frolov <frolov.frontend@gmail.com>
2021-05-07 12:03:13 +0300

    ci: vite aliases added for dev purposes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 const { createVuePlugin } = require('vite-plugin-vue2');
+const path = require('path');
 
 module.exports = {
-    plugins: [createVuePlugin()]
+    plugins: [createVuePlugin()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+            '@goodt/core': path.resolve(__dirname, '/src/core')
+        }
+    }
 };

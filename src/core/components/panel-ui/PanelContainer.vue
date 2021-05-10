81e761468ffcdbfb26f2c231bd9e2eb9cf946af2
S. Frolov <frolov.frontend@gmail.com>
2021-05-10 19:41:26 +0300

    refactor: Panel structure and typings revised

---
+++ src/core/components/panel-ui/PanelContainer.vue
@@ -0,0 +1,15 @@
+<template functional>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    /*
+    functional: true,
+    render(createElement, context) {
+        return context.$slots;
+    }
+    */
+};
+</script>

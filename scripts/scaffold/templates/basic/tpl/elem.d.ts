bbb940933f8ea53fa30d523d33d0941823865d24
S. Frolov <s.frolov@goodt.me>
2021-05-14 14:15:23 +0300

    refactor: typings refined

--- scripts/scaffold/templates/basic/tpl/elem.d.ts
+++ scripts/scaffold/templates/basic/tpl/elem.d.ts
@@ -2,16 +2,15 @@ import { IDescriptorProps } from '[[{core}]]';
 import Elem, { IElemComponentOptions, IElemInstance } from '[[{core}]]/Elem';
 import { descriptor } from './descriptor';
 [[#hasTransport]]
-import { TransportMixinComputed } from '[[{coreMixins}]]/useTransport';
+import { ITransportMixinInstance } from '[[{coreMixins}]]/useTransport';
 [[/hasTransport]]
 
 interface Data {}
 interface Methods {}
 interface Props {}
-interface Computed extends IDescriptorProps<ReturnType<typeof descriptor>>[[#hasTransport]],
-    TransportMixinComputed[[/hasTransport]] {}
+interface Computed extends IDescriptorProps<ReturnType<typeof descriptor>> {}
 
-export interface IInstance extends IElemInstance, Data, Methods, Computed, Props {}
-export interface IComponentOptions extends IElemComponentOptions<Data, Methods, Computed, Props> {
+export interface IInstance extends IElemInstance, [[#hasTransport]]ITransportMixinInstance,[[/hasTransport]] Data, Methods, Computed, Props {}
+export interface IComponentOptions extends IElemComponentOptions<IInstance, Data, Methods, Computed, Props> {
     extends: typeof Elem;
 }
--- src/dev/test/ElemImage/ElemImage.d.ts
+++ src/dev/test/ElemImage/ElemImage.d.ts
@@ -1,17 +1,17 @@
 import Elem, { IElemComponentOptions, IElemInstance } from '@goodt/core/Elem';
+import { ITransportMixinInstance } from '@goodt/core/mixins/useTransport';
 import { IDescriptorProps } from '@goodt/core';
 import { descriptor } from './descriptor';
 
-interface Methods {}
+interface Methods { }
 
-interface Computed {}
+interface Computed extends IDescriptorProps<ReturnType<typeof descriptor>> { }
 
-interface Data {}
+interface Data { }
 
-interface Props extends IDescriptorProps<ReturnType<typeof descriptor>> {}
+interface Props { }
 
-export interface IInstance extends IElemInstance, Data, Methods, Computed, Props {}
-
-export interface IComponentOptions extends IElemComponentOptions<Data, Methods, Computed, Props> {
+export interface IInstance extends IElemInstance, ITransportMixinInstance, Data, Methods, Computed, Props { }
+export interface IComponentOptions extends IElemComponentOptions<IInstance, Data, Methods, Computed, Props> {
     extends: typeof Elem;
 }

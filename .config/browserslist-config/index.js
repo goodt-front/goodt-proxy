87d8708e3d63f9a6d2c862acd1129e6b32a075a1
S. Frolov <s.frolov@goodt.me>
2021-05-19 20:08:23 +0300

    refactor: same browserslist for dev and prod envs

--- .config/browserslist-config/index.js
+++ .config/browserslist-config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    development: ['last 2 chrome version', 'last 2 firefox version'],
+    development: ['> 1%', 'last 2 versions', 'not ie > 0', 'not ie_mob > 0', 'edge >= 16'],
     production: ['> 1%', 'last 2 versions', 'not ie > 0', 'not ie_mob > 0', 'edge >= 16']
 };

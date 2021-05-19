fa04186c73375b1fbb7bdddcac7174ecd6f37c18
S. Frolov <s.frolov@goodt.me>
2021-05-19 18:40:01 +0300

    ci: shareable @goodt/browserslist-config added

---
+++ .config/browserslist-config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    development: ['last 2 chrome version', 'last 2 firefox version'],
+    production: ['> 1%', 'last 2 versions', 'not ie > 0', 'not ie_mob > 0', 'edge >= 16']
+};

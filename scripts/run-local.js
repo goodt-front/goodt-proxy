7f12be1077a0f95d3035c79794d7b55bf3f3ee2f
S. Frolov <s.frolov@goodt.me>
2021-05-20 19:29:54 +0300

    ci: script exit codes set to 0

--- scripts/run-local.js
+++ scripts/run-local.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const isLocalInstall = fs.existsSync(path.resolve(__dirname, '../package-lock.json'));
 if (isLocalInstall === false) {
-    process.exit(1);
+    process.exit(0);
 }
-const scripts = process.argv.slice(2);
+const scripts = process.argv.splice(-1);
 if (scripts.length === 0) {
-    process.exit(1);
+    process.exit(0);
 }
 // we need to run our script, so we need to run a new process
 const npm = require('npm-commands');
 scripts.forEach((scriptName) => {
-    npm().runAsync(scriptName);
+    npm()
+        .cwd(path.resolve(__dirname, '..'))
+        .runAsync(scriptName);
 });

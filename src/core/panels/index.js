451ad890dd772776eaf6ab57c623e091a1c42092
S. Frolov <s.frolov@goodt.me>
2021-05-26 14:16:41 +0300

    refactor: modules import/export revisions done

--- src/core/panels/index.js
+++ src/core/panels/index.js
@@ -1,7 +1,3 @@
-export { default as DremioPanel } from './DremioPanel.vue';
-export { default as StylePanel } from './StylePanel.vue';
-export { default as VariablePanel } from './VariablePanel.vue';
-
 const DremioPanelAsync = () => import('./DremioPanel.vue');
 const StylePanelAsync = () => import('./StylePanel.vue');
 const VariablePanelAsync = () => import('./VariablePanel.vue');

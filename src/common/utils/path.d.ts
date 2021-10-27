9f73e31ac4c0b12be8ec951a8ee19890bdc0cdc4
S. Frolov <s.frolov@goodt.me>
2021-10-27 13:23:02 +0300

    enh() - `joinPathParts` functionality enhanced to be identical to path.join()

---
+++ src/common/utils/path.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Join url path parts resolving leading and trailing slashes problem
+ *
+ * @param {string} args
+ * @return {string}
+ */
+export function joinPathParts(...args: string[]): string;
--- src/common/utils/path.js
+++ src/common/utils/path.js
@@ -1,14 +1,42 @@
 const SCHEMA_DELIMITER = '://';
 
 /**
+ * @param {string} source
+ * @param {string} [replaceLeading='']
+ * @param {string} [replaceTrailing='']
+ * @return {*}
+ */
+const trimSlashes = (source, replaceLeading = '', replaceTrailing = '') => {
+    return source.replace(/^\/+/, replaceLeading).replace(/\/+$/, replaceTrailing);
+};
+
+/**
  * Join url path parts resolving leading and trailing slashes problem
  *
- * @param {string[]} args
- * @return {string|*}
+ * @param {string} args
+ * @return {string}
  */
 export const joinPathParts = (...args) => {
     const [base, ...rest] = args;
-    const join = (...jargs) => jargs.map((str) => str.replace(/^\//, '').replace(/\/$/, '')).join('/');
+
+    // реализация совпадающая с node path.join() по поведению
+    const join = (...parts) => {
+        const joinedPath = [];
+        const [first, ...restParts] = parts;
+        const [last] = restParts.splice(-1);
+
+        if (first != null) {
+            joinedPath.push(trimSlashes(first, '/'));
+        }
+        if (restParts.length > 0) {
+            joinedPath.push(...restParts.map((part) => trimSlashes(part)));
+        }
+        if (last != null) {
+            joinedPath.push(trimSlashes(last, '', '/'));
+        }
+
+        return joinedPath.join('/');
+    };
 
     if (base.includes(SCHEMA_DELIMITER)) {
         const [protocol, restUrlPart] = base.split(SCHEMA_DELIMITER);

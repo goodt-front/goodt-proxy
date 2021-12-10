955d01d64e77c54a980c1840318d358d43d7f233
S. Frolov <s.frolov@goodt.me>
2021-12-10 16:26:31 +0300

    enh(utils): url utility added

---
+++ src/common/utils/url.d.ts
@@ -0,0 +1,57 @@
+type UrlPartQuery = Record<string, string | number | boolean | null | undefined>;
+
+interface UrlPartsExtra {
+    /**
+     * Key-value object with search string pairs key=value
+     */
+    query: UrlPartQuery;
+
+    /**
+     * Alias for URL's `pathname` to meet RouteManager Public Interface
+     */
+    path: string;
+}
+
+interface UrlInstance extends UrlPartsExtra, URL {
+    /**
+     * Indicates if url includes host parts
+     */
+    isAbsolute: boolean;
+
+    /**
+     * Indicates if url not includes host parts
+     */
+    isRelative: boolean;
+}
+
+/**
+ * Type for input object
+ */
+type UrlParts = Partial<UrlPartsExtra> & Partial<Omit<URL, 'toString' | 'toJSON'>>;
+
+/**
+ * Utility class – window.URL wrapper with safe constructor.
+ * Allows create URL instance without origin/host/protocol
+ **/
+export class Url implements UrlInstance {
+    /**
+     * Static class factory
+     *
+     * @param {UrlParts | string} input
+     * @return {string}
+     */
+    static create(input: UrlParts | string = ''): Url;
+
+    /**
+     * Concats path part strings, resolving leading and trailing slashes
+     *
+     * @param {...string[]} args
+     * @return {string}
+     */
+    static join(...args: string[]): string;
+
+    /**
+     * @construct {Url}
+     */
+    constructor(input: UrlParts | string = ''): Url;
+}
